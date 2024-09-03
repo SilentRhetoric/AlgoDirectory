191449c607b4f91005392477cc34372215fe56fa
SilentRhetoric <dev@silentrhetoric.com>
2024-09-02 22:53:21 -0400

    Added repo boilerplate, Prettier, ESLint, and beginnings of site structure

---
+++ src/types/listing-types.ts
@@ -0,0 +1,15 @@
+export interface Listing {
+  address: string
+  avatar: string
+  banner: string
+  bio: string
+  discord: string
+  domain: string
+  email: string
+  name: string
+  owner: string
+  telegram: string
+  twitter: string
+  url: string
+  website: string
+}
