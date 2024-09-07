a4d7ab0ceac452eda4e6ff377fc8fea273bac51a
Brian Whippo <brian.whippo@algorand.foundation>
2024-09-07 17:49:23 -0400

    404 Not Found page update

--- src/routes/about.tsx
+++ src/routes/about.tsx
@@ -2,10 +2,9 @@ import SiteTitle from "~/components/SiteTitle"
 
 export default function About() {
   return (
-    <main class="mx-auto p-4 text-center text-gray-700">
+    <main class="text-gray-700 mx-auto p-4 text-center">
       <SiteTitle>About</SiteTitle>
-
-      <h1 class="max-6-xs my-16 text-6xl font-thin uppercase text-sky-700">About Page</h1>
+      <h1 class="max-6-xs text-sky-700 my-16 text-6xl font-thin uppercase">About Page</h1>
     </main>
   )
 }
